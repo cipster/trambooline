6e3acc827f323fac070bd42ba682a59e714e9228
cipster <bostanciprian@gmail.com>
2018-04-27 14:35:25 +0300

    Initial commit

---
+++ src/main/java/com/rainsoftware/trambooline/TramboolineApplication.java
@@ -0,0 +1,12 @@
+package com.rainsoftware.trambooline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TramboolineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TramboolineApplication.class, args);
+    }
+}
